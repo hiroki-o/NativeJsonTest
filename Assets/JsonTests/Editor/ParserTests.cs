3c623397f78f9514c6e1f714abcaaaf331002156
Hiroki Omae <pegion6@gmail.com>
2014-01-20 15:58:12 +0100

    simple tests and examples

---
+++ Assets/JsonTests/Editor/ParserTests.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace NativeJsonTest
+{
+	[TestFixture]
+	public class ParserTests : MonoBehaviour
+	{
+//		[Datapoint]
+//		public double zero = 0;
+		[SerializeField]
+		public TextAsset testInput1;
+
+		[Datapoint]
+		public string value1 = "Hello Json.";
+		[Datapoint]
+		public string valueKey1 = "The quick brown fox jumps over the lazy dog";
+		[Datapoint]
+		public int valueKey2 = 456789;
+		[Datapoint]
+		public double value2 = 123.45;
+		[Datapoint]
+		public string[] value3 = {"A", "B", "C"};
+
+		[Test]
+//		[Ignore ("Ignored test")]
+//		[ExpectedException (typeof (ArgumentException), ExpectedMessage = "expected message")]
+		public void Parse ()
+		{
+			Json json = new Json();
+			json.ParseDocument(testInput1.text);
+		}
+
+		[Test]
+		public void GetString ()
+		{
+			Json json = new Json();
+			json.ParseDocument(testInput1.text);
+
+			// test direct query
+			Assert.That ( json.IsString ("name1") );
+			Assert.That ( json.GetString ("name1").Equals(value1) );
+
+			JsonValue jv = json.GetValue("name1");
+			Assert.That ( jv.isString );
+			Assert.That ( jv.stringValue.Equals(value1) );
+		}
+
+		[Test]
+		public void IsNotOtherThanString ()
+		{
+			Json json = new Json();
+			json.ParseDocument(testInput1.text);
+			
+			// test direct query
+			Assert.That ( !json.IsArray ("name1") );
+			Assert.That ( !json.IsBool ("name1") );
+			Assert.That ( !json.IsDouble ("name1") );
+			Assert.That ( !json.IsInt ("name1") );
+			Assert.That ( !json.IsNull ("name1") );
+			Assert.That ( !json.IsNumber ("name1") );
+			Assert.That ( !json.IsObject ("name1") );
+
+			JsonValue jv = json.GetValue("name1");
+			Assert.That ( !jv.isArray );
+			Assert.That ( !jv.isBool );
+			Assert.That ( !jv.isDouble );
+			Assert.That ( !jv.isInt );
+			Assert.That ( !jv.isNull );
+			Assert.That ( !jv.isNumber );
+			Assert.That ( !jv.isObject );
+		}
+	}
+}
---
+++ Assets/JsonTests/JsonExample.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System;
+using System.Threading;
+
+public class JsonExample : MonoBehaviour
+{
+	internal class Assert {
+		public static void That(bool b) {
+			Debug.Log ("Result:"+b);
+		}
+	}
+
+	[SerializeField]
+	public TextAsset testInput1;
+
+	public string value1 = "Hello Json.";
+	public string valueKey1 = "The quick brown fox jumps over the lazy dog";
+	public int valueKey2 = 456789;
+	public double value2 = 123.45;
+	public string[] value3 = {"A", "B", "C"};
+
+	void Start() {
+		Debug.Log ("Parse");
+		Parse();
+		Debug.Log ("GetString");
+		GetString();
+		Debug.Log ("IsNotOtherThanString");
+		IsNotOtherThanString();
+	}
+
+	public void Parse ()
+	{
+		Json json = new Json();
+		json.ParseDocument(testInput1.text);
+	}
+
+	public void GetString ()
+	{
+		Json json = new Json();
+		json.ParseDocument(testInput1.text);
+
+		// test direct query
+		Assert.That ( json.IsString ("name1") );
+		Assert.That ( json.GetString ("name1").Equals(value1) );
+
+		JsonValue jv = json.GetValue("name1");
+		Assert.That ( jv.isString );
+		Assert.That ( jv.stringValue.Equals(value1) );
+	}
+
+	public void IsNotOtherThanString ()
+	{
+		Json json = new Json();
+		json.ParseDocument(testInput1.text);
+		
+		// test direct query
+		Assert.That ( !json.IsArray ("name1") );
+		Assert.That ( !json.IsBool ("name1") );
+		Assert.That ( !json.IsDouble ("name1") );
+		Assert.That ( !json.IsInt ("name1") );
+		Assert.That ( !json.IsNull ("name1") );
+		Assert.That ( !json.IsNumber ("name1") );
+		Assert.That ( !json.IsObject ("name1") );
+
+		JsonValue jv = json.GetValue("name1");
+		Assert.That ( !jv.isArray );
+		Assert.That ( !jv.isBool );
+		Assert.That ( !jv.isDouble );
+		Assert.That ( !jv.isInt );
+		Assert.That ( !jv.isNull );
+		Assert.That ( !jv.isNumber );
+		Assert.That ( !jv.isObject );
+	}
+}
